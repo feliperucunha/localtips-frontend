77c927ce6caf11f08104cb4a1f1b335eebdd1187
Felipe Ruben Costa da Cunha <felipe.cunha@compasso.com.br>
2021-10-06 09:31:21 -0300

    api links

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   // Teste na API local
-  baseURL: 'http://localhost:3333',
+  //baseURL: 'http://localhost:3333',
   // Conecta na API do Heroku
-  //baseURL: 'https://localtips.herokuapp.com',
+  baseURL: 'https://localtips.herokuapp.com',
 });
 
 export default api;
