1da02d5a84f8180f19ebe5998dc8ea003e2bf10e
Felipe Ruben Costa da Cunha <felipe.cunha@compasso.com.br>
2021-10-12 18:24:54 -0300

    adiciona estilo no componente loading

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import '../styles/components/loadingPage.css';
 
 export default function LoadingPage() {
 
   return (
-    <div>
-      <h1>Carregando</h1>
+    <div className="Loading__Page">
+      <h1>Carregando...</h1>
     </div>
   );
 }
